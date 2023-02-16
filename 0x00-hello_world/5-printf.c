73627eae9c5cdbe802761e389f0b2f2c4fcd0ce6
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-16 15:40:51 -0800

    new commit 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function to print text
+ *
+ * Return: text
+ */
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
