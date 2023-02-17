6c3c0472c883e59908868b7177324f2cdaceddb8
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-17 13:57:40 -0800

    tebahpla commit 1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return value
+*/
+
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
