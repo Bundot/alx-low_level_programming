afda89ffd1d75fe67488000fd0b57771638c4c00
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 17:38:19 -0800

    n c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,9 @@
 
 /**
 * print_sign - main function
-* @c: print 
 * 
+* @n: variable
+*
 * Return: return value
 */
 
@@ -14,7 +15,7 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return 1;
+return (1);
 }
 else if (n == 0)
 {
