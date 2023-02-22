7a770a868d83a2b36fbc6f6dd88e17ebd8b5305b
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 17:44:57 -0800

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _abs - main function
+*
+* @n: variable
+*
+* Return: return value
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return n;
+}
+}
+
