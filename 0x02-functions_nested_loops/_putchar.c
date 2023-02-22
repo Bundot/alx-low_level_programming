e67b39c47d3143ecc1a8e4a20154e00670263491
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 17:06:01 -0800

    new commit 3

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,1 +1,13 @@
-typedef int make_iso_compilers_happy;
+#include <unistd.h>
+
+/**
+ * _putchar - writes the letter or number to be printed to  stdout
+ * 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately. Read More on This
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
