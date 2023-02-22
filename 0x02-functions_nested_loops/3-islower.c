5a0c179cf44d3013f2686b75687ee23e3f298eb7
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 16:54:53 -0800

    new commit 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _islower - main function
+*
+* Return: return value
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return 1;
+}
+else
+{
+return 0;
+}
+}
