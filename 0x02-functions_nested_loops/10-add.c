c4abc0a81f6075f52d2a9785819fa6a0dc9842d2
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 18:34:12 -0800

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* add - main function
+*
+* @a : first variable
+*
+* @b : second variable
+*
+* @n : add variable
+*
+* Return: return value
+*/
+
+int add(int a, int b)
+{
+int n = a + b;
+return (n);
+}
