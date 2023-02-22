5f2eb07caebe8ce45ed87a50d9a5512f256d689e
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 16:41:40 -0800

    new commit 11

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 
 /**
-* main - main function
+* print_alphabet_x10 - main function
 *
 * Return: return value
 */
 
-void print_alphabet_x10(void) {
-  char c;
-  int i;
-  for (i = 0; i < 260; i++) {
-    c = 'a' + (i % 26);
-    putchar(c);
-    if (i % 26 == 25) {
-      putchar('\n');
-    }
-  }
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+for (i = 0; i < 260; i++)
+{
+c = 'a' + (i % 26);
+putchar(c);
+if (i % 26 == 25)
+{
+putchar('\n');
+}
+}
 }
