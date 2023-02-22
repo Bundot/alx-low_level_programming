6de21bc22ed3ca5afbcda56fe3c25cfdc5200200
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 16:01:39 -0800

    new commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-/**
-* main - main function
-*
-* Return: return value
-*/
 
 void print_alphabet(void)
 {
-char c = 'a';  
-while (c <= 'z')
-{
-putchar(c);
-c++;
-}
-putchar('\n');
-}
-int main(void)
-{
-print_alphabet();
-return (0);
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        putchar(c);
+    }
+    putchar('\n');
 }
