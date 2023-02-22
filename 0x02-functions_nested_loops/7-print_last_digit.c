3b6744abcba3e2299259b8a2e32823ab9e6eb9c8
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-21 17:57:36 -0800

    new commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* print_last_digit - main function
+*
+* @n: variable
+*
+* Return: return value
+*/
+
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+if (last_digit < 0)
+{
+last_digit = -last_digit;
+}
+_putchar(last_digit + '0');
+return (last_digit);
+}
