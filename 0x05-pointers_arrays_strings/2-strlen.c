7230efb41a91e80fc80cc46d53210ec522d81ac9
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-27 05:32:08 -0800

    strlen commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strlen - main function
+* @s: variable
+*
+* Return: return value
+*/
+
+#include <stdio.h>
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return len;
+}
