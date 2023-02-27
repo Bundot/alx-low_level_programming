92b1c24630768537072818feaecf41330aa26e47
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-27 14:09:26 -0800

    new commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* puts_half - main function
+* @str: variable
+*
+* Return: return value
+*/
+
+void puts_half(char *str){
+size_t len;
+size_t offset;
+if (str == NULL)
+return;
+len = strlen(str);
+offset = (len + 1) / 2;
+ while (offset < len)
+{
+_putchar(str[offset]);
+offset++;
+}
+_putchar('\n');
+}
