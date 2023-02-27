564d72f4f375f3a65736b155a4cc9ccb5d83b7f6
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-27 05:51:45 -0800

    newest commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _puts - main function
+* str: variable
+*
+* Return: return value
+*/
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
