c3bcc96709ab8de6d5d6c5dbddbed5efc69ea93d
Isaac Dauda <isaacdauda12@gmail.com>
2023-02-27 03:19:50 -0800

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* reset_to_98 - main function
+* @*n: variable
+*
+* Return: return value
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
