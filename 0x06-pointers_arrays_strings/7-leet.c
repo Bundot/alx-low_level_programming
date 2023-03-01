a786ce2c78ff7bfafd00b38ab8236a508eed1626
Isaac Dauda <isaacdauda12@gmail.com>
2023-03-01 10:08:25 -0800

    new commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * leet - main function
+ * @str - variable
+ *
+ * Return: return value
+ */
+
+char *leet(char *str)
+{
+char *ptr = str;
+char *leet_chars = "aAeEoOtTlL";
+char *leet_nums = "4433007711";
+int i;
+while (*ptr)
+{
+i = 0;
+while (leet_chars[i])
+{
+if (*ptr == leet_chars[i])
+{
+*ptr = leet_nums[i];
+break;
+}
+i++;
+}
+ptr++;
+}
+return (str);
+}
