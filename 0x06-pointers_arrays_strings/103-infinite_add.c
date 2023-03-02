ba8b43bf8daaf67c3fcb07d2c16826e50076b91b
Isaac Dauda <isaacdauda12@gmail.com>
2023-03-02 14:05:25 -0800

    new commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
- * 
- * */
+* _strcmp - main function
+* @n1: variable
+* @n2: variable
+* @r: variable
+* @size_r: variable
+*
+* Return: return value
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int carry = 0;
+int i = 0;
+int digit;
+int digit1;
+int digit2;
+int sum;
+while (*n1 != '\0' || *n2 != '\0' || carry != 0)
+{
+digit1 = (*n1 != '\0') ? *n1 - '0' : 0;
+digit2 = (*n2 != '\0') ? *n2 - '0' : 0;
+sum = digit1 + digit2 + carry;
+carry = (sum >= 10) ? 1 : 0;
+digit = sum % 10;
+if (i >= size_r - 1)
+{
+return (0);
+}
+r[i++] = digit + '0';
+n1++;
+n2++;
+}
+r[i] = '\0';
+return (r);
+}
